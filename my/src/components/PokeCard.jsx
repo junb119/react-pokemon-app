2371873021a454f703c49ea226ba2dd3a5044606
jun <emfowkd@gmail.com>
2024-12-06 15:32:52 +0900

    1-10 포켓몬 카드 UI 생성하기

--- my/src/components/PokeCard.jsx
+++ my/src/components/PokeCard.jsx
@@ -28,7 +28,46 @@ const PokeCard = ({ url, name }) => {
     };
     return PokeData; //포멧팅한 데이터 반환
   }
-  return <div>PokeCard</div>;
+
+  const bg = `bg-${pokemon?.type}`; // pokemon?.type : pokemon이 undefined가 아닐때, 즉 존재할 때 type을 가져옴(optional)
+  const border = `border-${pokemon?.type}`;
+  const text = `text-${pokemon?.type}`;
+  const img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon?.id}.png`;
+  return (
+    <>
+      {pokemon && (
+        <a
+          // aria-label={name}
+          href={`/pokemon/${name}`}
+          className={`box-border rounded-lg ${border} w-[8.5rem] h-[8.5rem] z-0 bg-slate-800 justify-between items-center`}
+        >
+          <div
+            className={`${text} basis h-[1.5rem] text-xs w-full pt-1 px-2 text-right rounded-t-lg`}
+          >
+            #{pokemon.id.toString().padStart(3, "00")}
+            {/* 001,002 같은 포멧으로 나오도록 */}
+          </div>
+          <div className={`w-full f-6 flex items-center justify-center`}>
+            <div
+              className={`box-border relative flex w-full h-[5.5rem] basis justify-center items-center`}
+            >
+              <img
+                src={img}
+                alt={name}
+                width="100%"
+                className={`object-contain h-full`}
+              />
+            </div>
+          </div>
+          <div
+            className={`${bg} text-xs text-zinc-100 h-[1.5rem] rounded-b-lg uppercase font-medium pt-1 `}
+          >
+            {pokemon.name}
+          </div>
+        </a>
+      )}
+    </>
+  );
 };
 
 export default PokeCard;
