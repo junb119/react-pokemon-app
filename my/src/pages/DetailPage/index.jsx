4958e651940e20f0098461d09dd49c9431544cf8
jun <emfowkd@gmail.com>
2024-12-07 22:51:58 +0900

    2-5 포켓몬 데이터 가공하기

--- my/src/pages/DetailPage/index.jsx
+++ my/src/pages/DetailPage/index.jsx
@@ -1,12 +1,14 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const DetailPage = () => {
   const [pokemon, setPokemon] = useState();
+  const [isLoading, setIsLoading] = useState(false); // 비동기작업 완료까지 표시할 로딩 state
   const params = useParams(); // path에 있는 포켓몬 id를 가져오기 위한 useParams
   const baseURL = `https://pokeapi.co/api/v2/pokemon/`;
   useEffect(() => {
+    setIsLoading(true);
     fetchPokemonData(params.id);
   }, [params]);
 
@@ -23,11 +25,61 @@ const DetailPage = () => {
         console.log("pokemonData", pokemonData);
         const nextAndPreviousPokemon = await getNextAndPreviousPokemon(id); // 이전, 다음 포켓몬데이터 가져오기
         console.log(nextAndPreviousPokemon);
+
+        const DamageRelations = await Promise.all(
+          // 데미지 관계데이터 가져오기
+          // Promis.all : 비동기작업들을 다 작업한 후 한번에 리턴
+          types.map(async (i) => {
+            const type = await axios.get(i.type.url);
+            return type.data.damage_relations;
+          })
+        );
+        const formattedPokemonData = {
+          // 포켓몬 데이터 가공하기
+          id,
+          name,
+          abilities: formatPokemonAbilities(abilities),
+          DamageRelations,
+          weight: weight / 10,
+          height: height / 10,
+          stats: formatPokemonStats(stats),
+          types: types.map((i) => i.type.name),
+          previous: nextAndPreviousPokemon.previous,
+          next: nextAndPreviousPokemon.next,
+        };
+
+        console.log("formattedPokemonData", formattedPokemonData);
+        setPokemon(formattedPokemonData);
+        setIsLoading(false);
       }
     } catch (error) {
       console.error(error);
     }
   }
+  const formatPokemonAbilities = (Abilities) =>
+    // Ability 데이터를 포멧하기
+    Abilities.filter((_, index) => index <= 1).map(
+      (
+        obj // index가 1이하인 ability데이터만 가져오기
+      ) => obj.ability.name.replaceAll("-", " ") // 모든 -를 공백으로 변경
+    );
+  const formatPokemonStats = ([
+    // 스텟 데이터 가공하기
+    statHP,
+    statATK,
+    statDEF,
+    statSATK,
+    statSDEP,
+    statSPD, //구조분해로 데이터가져오기
+  ]) => [
+    { name: "Hit Points", baseStat: statHP.base_stat },
+    { name: "Attack", baseStat: statATK.base_stat },
+    { name: "Defense", baseStat: statDEF.base_stat },
+    { name: "Special Attack", baseStat: statSATK.base_stat },
+    { name: "Special Defense", baseStat: statSDEP.base_stat },
+    { name: "Speed", baseStat: statSPD.base_stat },
+  ];
+
   async function getNextAndPreviousPokemon(id) {
     // 이전, 다음 포켓몬데이터 가져오기
     const urlPokemon = `${baseURL}?limit=1&offset=${id - 1}`; // 기준이 되는 포켓몬 데이터 가져오기; 만약 offset=5(6-1)이면 5에서 limit=1을 가져옴 => 6번째 포켓몬
@@ -44,6 +96,8 @@ const DetailPage = () => {
       previous: previousResponse?.data?.results?.[0]?.name,
     };
   }
-};
 
+  if (isLoading) return <div>...loading</div>; // isLoading state가 true면 로딩애니메이션 렌더링
+  return <div>DetailPage</div>;
+};
 export default DetailPage;
