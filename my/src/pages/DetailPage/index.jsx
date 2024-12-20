f976c74d5d7fff9f61bbd6f4eaeb37bf2787bd07
jun <emfowkd@gmail.com>
2024-12-20 20:41:47 +0900

    2-8 BaseStat 컴포넌트 생성하기

--- my/src/pages/DetailPage/index.jsx
+++ my/src/pages/DetailPage/index.jsx
@@ -8,6 +8,7 @@ import { ArrowLeft } from "../../assets/ArrowLeft";
 import { Vector } from "../../assets/Vector";
 import { Balance } from "../../assets/Balance";
 import { Type } from "../../components/Type";
+import BaseStat from "../../components/BaseStat";
 
 const DetailPage = () => {
   const [pokemon, setPokemon] = useState();
@@ -211,7 +212,24 @@ const DetailPage = () => {
             </div>
           </div>
           <h2 className={`text-base font-semibold ${text}`}>기본 능력치</h2>
-          <div className="w-full">{/* stats */}</div>
+          <div className="w-full">
+            {/* stats */}
+
+            <table>
+              <tbody>
+                {pokemon.stats.map((stat) => {
+                  return (
+                    <BaseStat
+                      key={stat.name}
+                      valueStat={stat.baseStat}
+                      nameStat={stat.name}
+                      type={pokemon.types[0]}
+                    />
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
           {pokemon.DamageRelations && (
             <div className="w-10/12">
               <h2
